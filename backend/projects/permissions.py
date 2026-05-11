c2d40a6f0c91799d68d966127dcf55fe2279cf7e
unknown <stefanpopovicnew@gmail.com>
2026-05-11 20:08:44 +0200

    feat(projects) implement custom permission that depends on Task creator / assignee

--- backend/projects/permissions.py
+++ backend/projects/permissions.py
@@ -1,9 +1,11 @@
 from rest_framework import permissions
+from django.shortcuts import get_object_or_404
+from .models import Project
 
 class IsCreatorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
-                    return True
+            return True
         return obj.owner == request.user
 
 class IsInviteParticipant(permissions.BasePermission):
@@ -18,5 +20,30 @@ class IsInviteParticipant(permissions.BasePermission):
 
         if view.action in ["destroy", "cancel"]:
             return obj.invited_by == user
+
+        return False
+    
+class IsTaskCreatorOrAssignee(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if view.action == 'create':
+            project_id = request.data.get('project')
+            project = get_object_or_404(Project, id=project_id)
+            return project.owner == request.user
+        return True
+
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        is_creator = obj.created_by == user
+        is_assignee = obj.assignee == user
+
+        if view.action == 'retrieve':
+            return is_creator or is_assignee
+
+        if view.action in ['update', 'partial_update', 'destroy', 'create_checklist_item']:
+            return is_creator
+
+        if view.action in ['add_comment', 'update_checklist_item', 'update_task_status']:
+            return is_creator or is_assignee
 
         return False
